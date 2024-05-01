75b59b3170c69e72c6ef9369b7152235d39d66a0
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 15:50:25 -0500

    removed comma at end

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,9 @@
 for i in range(10):
     for j in range(i + 1,10):
         if i != j:
-            combination = f"{i}{j}, "
+            combination = f"{i}{j}"
             print("{}".format(combination), end="")
+            if combination != "89":
+                    print(", ", end="")
+            else:
+                    print("\n", end="")
