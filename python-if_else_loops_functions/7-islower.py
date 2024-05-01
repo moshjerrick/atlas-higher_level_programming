06550ae5df962569879020a36832d303b65f1ede
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 17:04:00 -0500

    fixed some syntax and added new line

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
     ascii_value = ord(c)
-    if 97 <= ascii_value <= 122:
-    print("True" if_lower else "False", end="")
+    is_lower = 97 <= ascii_value <= 122
+    print("True" if is_lower else "False", end="")
