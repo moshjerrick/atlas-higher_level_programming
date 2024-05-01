75ce127c9e8384e46d3e187b609521c5e1e55c1b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 15:51:27 -0500

    fixed new bettys issues

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i + 1,10):
+    for j in range(i + 1, 10):
         if i != j:
             combination = f"{i}{j}"
             print("{}".format(combination), end="")
             if combination != "89":
-                    print(", ", end="")
+                print(", ", end="")
             else:
-                    print("\n", end="")
+                print("\n", end="")
