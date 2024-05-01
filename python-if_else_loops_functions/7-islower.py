d62126816f67cb743904719a18a58ee27c910cf3
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 17:07:17 -0500

    renamed things

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
     ascii_value = ord(c)
-    is_lower = 97 <= ascii_value <= 122
-    print("True" if is_lower else "False", end="")
+    c = 97 <= ascii_value <= 122
+    print("True" if (c) else "False", end="")
