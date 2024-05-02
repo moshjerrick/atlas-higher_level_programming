85d2e9bc12866940fbbcbd5e339aca48aafe0b4c
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 19:11:01 -0500

    adding 12, dont think it works

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizzbuzz")
+        elif 1 % 3 == 0:
+            print("Fizz")
+        elif 1 % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
