356386998b262839b5b17ba828862dbd8bba4cac
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 12:10:40 -0500

    fixed indents

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import calculator
-    a = 10
-    b = 5
-    def add(a, b):
-        print("{} + {} = {}".format(a, b, add(a, b)))
+from calculator_1 import calculator
+a = 10
+b = 5
+def add(a, b):
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-    def subtract(a, b):
-        print("{} - {} = {}".format(a, b, subtract(a, b)))
+def subtract(a, b):
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
 
-    def multiply(a, b):
-        print("{} * {} = {}".format(a, b, multiply(a,b)))
+def multiply(a, b):
+    print("{} * {} = {}".format(a, b, multiply(a,b)))
 
-    def divide(a, b):
-        print("{} / {} = {}".format(a, b, divide(a, b)))
+def divide(a, b):
+    print("{} / {} = {}".format(a, b, divide(a, b)))
+
+    if __name__ == "__main__":
