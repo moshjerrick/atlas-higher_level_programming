f0f54099101b0978d8b603d5da2c7503a97cbb0b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:19:44 -0500

    enumerates

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
     print("{} {}".format(arg_number, "argument: "" if arg_number == 1 else "s"))
-    for arg in sys.argv[1:]:
-        print("{}".format(arg))
+    for i, arg in enumerate(sys.argv[1:], start = 1):
+        print("{}. {}".format(i, arg))
