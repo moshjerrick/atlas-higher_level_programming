0addee34a44ddeec88dd12a682a33244bda9a4ce
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 12:06:09 -0500

    pushing for posterity

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import calculator
+    a = 10
+    b = 5
+    def add(a, b):
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    def subtract(a, b):
+        print("{} - {} = {}".format(a, b, subtract(a, b)))
+    def multiply(a, b):
+        print("{} * {} = {}".format(a, b, multiply(a,b)))
+    def divide(a, b):
+        print("{} / {} = {}".format(a, b, divide(a, b)))
