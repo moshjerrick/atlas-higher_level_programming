867e0e3f76355a8ad565c90e12db7cf7a86872ec
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:27:51 -0500

    fixed syntax

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
-    print("{} {}".format(arg_number, "argument:", ""if arg_number == else "s"))
+    print("{} {}".format(arg_number, "argument:" if arg_number == 1 else "arguments:"))
     for i, arg in enumerate(sys.argv[1:], start = 1):
         print("{}: {}".format(i, arg))
