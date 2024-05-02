981aee0bcbce5a717d77c9d378345437a970d8e3
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 12:36:07 -0500

    added space between b and =

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, subtract, multiply, divide
 
     a = 10
-    b= 5
+    b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, subtract(a, b)))
