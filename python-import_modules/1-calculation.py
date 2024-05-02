be97a94b2db067cd72bef1c178c5d6ea2f638a16
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 12:15:07 -0500

    returning, instead of string printing

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,15 +3,11 @@ from calculator_1 import calculator
 a = 10
 b = 5
 def add(a, b):
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
+    return calculator(a, b, '+')
 def subtract(a, b):
-    print("{} - {} = {}".format(a, b, subtract(a, b)))
-
+    return calculator(a, b, '-')
 def multiply(a, b):
-    print("{} * {} = {}".format(a, b, multiply(a,b)))
-
+    return calculator(a, b, '*')
 def divide(a, b):
-    print("{} / {} = {}".format(a, b, divide(a, b)))
-
+    return calculator(a, b, '/')
 if __name__ == "__main__":
