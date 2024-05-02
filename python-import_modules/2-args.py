e4efd964cfcf15de76b2d5bf10209c3d1a6fe31c
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:40:53 -0500

    added else

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
     if arg_number == 0:
             print("{}".format("0 arguments."))
+    else:
     print("{} {}".format(arg_number, "argument:" if arg_number == 1 else "arguments:"))
     for i, arg in enumerate(sys.argv[1:], start = 1):
         print("{}: {}".format(i, arg))
