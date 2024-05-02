59711817e156a20622de47c3c52ee687cbcc84e7
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:16:40 -0500

    added s if multiple args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
-    print("{} {}".format(arg_number, "argument: "))
+    print("{} {}".format(arg_number, "argument: "" if arg_number == 1 else "s"))
     for arg in sys.argv[1:]:
         print("{}".format(arg))
