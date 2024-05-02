c61b342305ddf6652ebf106160ab9264c7b1b30f
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:21:39 -0500

    changed . to :

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
     print("{} {}".format(arg_number, "argument:", ""if arg_number == 1 else "s"))
     for i, arg in enumerate(sys.argv[1:], start = 1):
-        print("{}. {}".format(i, arg))
+        print("{}: {}".format(i, arg))
