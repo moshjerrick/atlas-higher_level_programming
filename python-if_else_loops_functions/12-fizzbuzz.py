cf5de95ce39c0a5edd5c332a0c07fad94e1fe1c2
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 19:17:41 -0500

    added end="" to prints to make it one line

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end="")
         elif i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end="")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end="")
         else:
-            print(i)
+            print(i, end="")
 
 fizzbuzz()
