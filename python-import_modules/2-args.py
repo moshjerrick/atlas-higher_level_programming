5567191dca4a0b1ec8cecf3b69398cc23a59f3e6
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:11:33 -0500

    added argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,3 +4,5 @@ import sys
 if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
     print("{} {}".format(arg_number, "argument: "))
+    for arg in sys.argv[1:]:
+        print("{}".format(arg))
