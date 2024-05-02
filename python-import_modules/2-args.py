1ea07523e3d6b908e3db5c6c9ce20885ec56de3b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:42:21 -0500

    indent fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,8 @@ import sys
 if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
     if arg_number == 0:
-            print("{}".format("0 arguments."))
+        print("{}".format("0 arguments."))
     else:
-    print("{} {}".format(arg_number, "argument:" if arg_number == 1 else "arguments:"))
+        print("{} {}".format(arg_number, "argument:" if arg_number == 1 else "arguments:"))
     for i, arg in enumerate(sys.argv[1:], start = 1):
         print("{}: {}".format(i, arg))
