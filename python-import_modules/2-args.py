f6f6b83fd4ea77de2a30ba32fd9ede3e6285c253
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-02 13:47:25 -0500

    formatting

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     arg_number = len(sys.argv) - 1
     if arg_number == 0:
         print("{}".format("0 arguments."))
+    elif arg_number == 1:
+        print("{} {}".format(arg_number, "argument:"))
     else:
-        print("{} {}".format(arg_number, "argument:" if arg_number == 1 else "arguments:"))
+        print("{} {}".format(arg_number, "arguments:"))
+
     for i, arg in enumerate(sys.argv[1:], start = 1):
         print("{}: {}".format(i, arg))
