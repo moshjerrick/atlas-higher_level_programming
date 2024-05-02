c18d8258faec1ed230fd20c2156e2b071f71a20a
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-01 19:15:27 -0500

    called function at end to execute, works but not all the way

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,12 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("Fizzbuzz")
-        elif 1 % 3 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
             print("Fizz")
-        elif 1 % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz")
         else:
             print(i)
+
+fizzbuzz()
