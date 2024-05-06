4364b52a0fd29474897b0118148689c70761b861
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 13:54:00 -0500

    str format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print(num, end=" ")
+            print("{}".format(num), end="")
         print()
