5fdce5b700e2eb7eb1b0771921fa50211be8b537
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 15:12:00 -0500

    added where i'm looping and appended true and false

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for x in my_list:
-        if x % 2:
-            return True
+    is_div = []
+
+    for x in range(len(my_list)):
+        if my_list[x] % 2 == 0:
+            is_div.append(True)
+        else:
+            is_div.append(False)
+        return is_div
