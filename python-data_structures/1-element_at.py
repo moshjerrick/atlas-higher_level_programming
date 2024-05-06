85fc049560f8f896038062f543359f296289a582
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 12:16:58 -0500

    moved print down

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for num in my_list:
-        print("{:d}".format(num))
     if idx < 0:
         return None
     if idx > my_list:
         return None
+    for num in my_list:
+        print("{:d}".format(num))
