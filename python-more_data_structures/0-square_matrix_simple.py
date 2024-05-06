9f804b29605c8028ef692d5230c7898b5f2ba196
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 16:12:23 -0500

    parenthis fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda x: x ** 2, row, matrix))))
+    new_matrix = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
 
     print("{}".format(new_matrix))
