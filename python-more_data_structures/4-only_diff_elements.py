0f50fed883a6ca495fb108421605be3b42cc182d
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 17:07:42 -0500

    using symettric difference

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if (set_1 or set_2):
-        return list(set_1 or set_2)
-    else:
-        return []
+    return list(set_1.symettric_difference(set_2))
