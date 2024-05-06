655b357b2ee10247bef20e66abd01170c0b38cb5
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 14:25:15 -0500

    listed all out and added them

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return new_tuple
+    a = tuple_a[0] if len(tuple_a) > 0 else 0
+    b = tuple_a[1] if len(tuple_a) > 1 else 0
+    c = tuple_b[0] if len(tuple_b) > 0 else 0
+    d = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    sum_1 = a + c
+    sum_2 = b + d
+
+    return(sum_1, sum)
