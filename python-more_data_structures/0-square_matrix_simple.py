deaf38de58b987bb2b10aa602111e126cf7b2578
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 16:17:57 -0500

    removed all that stuff i just added

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,3 @@ def square_matrix_simple(matrix=[]):
     new_matrix = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
 
     print("{}".format(new_matrix))
-    for row in new_matrix:
-        print("{}".format(row))
