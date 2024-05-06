e2bd47280d04bccd94e73c36c761b58ac1db5edc
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 16:43:53 -0500

    added space after new_list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list= []
+    new_list = []
     for x in my_list:
         if x == search:
             new_list.append(replace)
