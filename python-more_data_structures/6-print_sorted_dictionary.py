8b65728f1c117d945106c3728df057ddf8efc034
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 18:23:34 -0500

    using items()

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    myKeys = list(a_dictionary.keys())
-    myKeys.sort()
-    sorted_dict = {i: a_dictionary[i] for i in myKeys}
-
-    print(sorted_dict)
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    print (sorted_dict)
