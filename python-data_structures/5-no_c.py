962badf1fe3f5e9e772605bc34d44ead459d4a0a
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 13:26:19 -0500

    did proper return

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in my_string:
-        if i == "c":
-            return
-        elif i == "C":
-            return
+        if i == "c" or i == "C":
+            return None
         return my_string
