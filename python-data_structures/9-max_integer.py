cde49a1672a57626c1499f95fb2d1677eaeb86e8
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 14:59:06 -0500

    forgot what i changed....

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    answer = None
-    for i in my_list:
-        if i > answer:
-            answer = i
-    return answer
+    if my_list == []:
+        return None
+    m = my_list[0]
+    for x in my_list:
+        if x > m:
+            m = x
+            return m
