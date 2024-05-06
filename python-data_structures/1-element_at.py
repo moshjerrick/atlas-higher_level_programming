74bf8fc165c341b46a3f4e68887b1511309cb43e
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 12:28:57 -0500

    fixed format string argument

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,4 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    print("{:d}{:d}".format(idx, element_at(list, idx))
+    print("{:d}{:d}".format(idx, my_list[idx]))
