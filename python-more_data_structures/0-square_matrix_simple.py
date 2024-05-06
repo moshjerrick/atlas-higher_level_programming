5c4da699587408a81ca306cf4658a59375257d27
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 16:14:34 -0500

    added end="

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
 
-    print("{}".format(new_matrix))
+    print("{}".format(new_matrix), end="")
