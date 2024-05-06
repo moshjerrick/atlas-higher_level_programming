8ec8a5d7445684feb1515700545b3a48b78b681c
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-06 12:24:21 -0500

    corrected checking for outside of range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    if idx > my_list:
-        return None
-    for num in my_list:
-        print("{:d}".format(num))
+    print("{:d}".format(my_list[idx]))
