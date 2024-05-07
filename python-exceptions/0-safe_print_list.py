27676831fce8b070ae3f18c7461a3a4485c785dc
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 13:40:35 -0500

    removed new line, added exception type and modigied last print statement

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="\n")
+            print("{}".format(my_list[i]), end="")
             count += 1
-        except:
+        except IndexError:
             break
-    print("")
+    print()
     return count
