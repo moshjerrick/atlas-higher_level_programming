478e5a164c4874725b8efb3638a1fd43d058ec23
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 13:30:24 -0500

    removed exception type

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="\n")
             count += 1
-        except IndexError:
+        except:
             break
     return count
