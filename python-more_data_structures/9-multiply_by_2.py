1dc878e9cd0f1be141b1be84210cb18c67258baf
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 15:48:34 -0500

    created new dict first

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary 
-    for i in a_dictionary:
-        new_dict = 2 * a_dictionary[i]
-        return new_dict
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
