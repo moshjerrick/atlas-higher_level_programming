a341eb38b0e399bb2126cff2b54d44b3c795003e
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 15:57:22 -0500

    added if not found

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     keymax = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
+    return keymax
