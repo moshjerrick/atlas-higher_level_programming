1ae0c6446c499ec3d320668bce656bf02f2bd738
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 13:57:35 -0500

    changed what was printed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     result = my_list[:x]
-    print("{}".format(str(result)))
-
+    for i in range(x):
+        try:
+            print("{}".format(result))
+        except IndexError:
+            print()
+    return result
