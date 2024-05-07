c6d4fe052d6d07e9a18e74aeaf6d4e67dfba24d3
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 14:03:25 -0500

    added new line and changed exception

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     result = my_list[:x]
     for item in result:
         try:
-            print("{:d}".format(item))
-        except IndexError:
+            print("{:d}".format(item), end="")
+        except:
             print()
     return result
