a3cda4e6ea8e318268b1b75492bcace26af6dbb0
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-07 14:02:34 -0500

    added :d for those darn ints

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     result = my_list[:x]
     for item in result:
         try:
-            print("{}".format(item))
+            print("{:d}".format(item))
         except IndexError:
             print()
     return result
