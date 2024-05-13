8f8286e26269cafc39678ae8ffec462c615aafff
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:32:47 -0500

    changed another != to is not

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize the Square with size"""
 
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         """size must be an integer"""
         if size < 0:
