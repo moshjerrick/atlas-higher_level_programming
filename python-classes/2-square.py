a30b033ddbdb8b8c2782877ba5d06652707c3fa0
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 12:53:59 -0500

    interger not int

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
 
         if type(size) != int:
             raise TypeError
-        """size must be an int"""
+        """size must be an integer"""
         if size < 0:
             raise ValueError
         """size must be above 0"""
