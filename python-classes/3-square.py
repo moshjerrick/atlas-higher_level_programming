5658713409a6562f3641464748330f55640ed36e
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:19:47 -0500

    indent

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,13 +9,13 @@ class Square:
         """Initialize the Square with size"""
 
         elif type(size) != int:
-            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
         """size must be an integer"""
         elif size < 0:
-            raise ValueError("size must be >= 0")
+        raise ValueError("size must be >= 0")
         """size must be above 0"""
         else:
-                self.__size = size
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
