7e4c10abd09a5a979bfb1790a968fa40d10a51dd
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:13:29 -0500

    removed old line

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,7 +14,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         """size must be above 0"""
-        self.__size = size
 
     def area(self):
         return self.size * self.__size
