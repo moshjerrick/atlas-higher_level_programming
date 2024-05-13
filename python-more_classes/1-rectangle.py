851c9a228137e5335abc5c6ffd22bbcc42c6f619
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 16:55:42 -0500

    did width first instead

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,20 +8,7 @@ class Rectangle:
         self.height = height
         self.width = width
 
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
-    @property
+     @property
     def width(self):
         return self.__width
 
@@ -34,4 +21,17 @@ class Rectangle:
         else:
             self.__width = value
 
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
