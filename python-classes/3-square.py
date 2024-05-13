4add8f097a8ab4514d44006cc20e602b6e28bc48
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:31:19 -0500

    chat gpt told me my comments were not right, do we trust them?

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,10 +10,10 @@ class Square:
 
         if type(size) != int:
             raise TypeError("size must be an integer")
-        """size must be an integer"""
+        # size must be an integer
         elif size < 0:
             raise ValueError("size must be >= 0")
-        """size must be above 0"""
+        # size must be above 0
         else:
             self.__size = size
 
