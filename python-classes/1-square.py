64b5dce264c30b974d76c5ef488265de6c80aa43
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 12:44:59 -0500

    adding 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defining a class square with size."""
+
+
+class Square:
+    """This be Square"""
+
+    def __init__(self, size):
+        """Initialize the Square"""
+
+        self.__size = size
