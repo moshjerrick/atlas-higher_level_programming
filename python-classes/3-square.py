b8d7e96e39238848fdf3d17b5b55224db50c95bc
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:09:21 -0500

    added 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defining a class square with size."""
+
+
+class Square:
+    """This be Square"""
+
+    def __init__(self, size=0):
+        """Initialize the Square with size"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        """size must be an integer"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """size must be above 0"""
+        self.__size = size
+
+    def area(self):
+        return self.size * self.__size
+    
