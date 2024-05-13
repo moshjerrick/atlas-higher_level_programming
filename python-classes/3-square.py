717206760a1be1f8bd5f16325f1dbed600426002
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:29:28 -0500

    i dont know what i'm doing wrong and at this point i'm afraid to ask

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,14 +8,14 @@ class Square:
     def __init__(self, size=0):
         """Initialize the Square with size"""
 
-        elif type(size) != int:
-        raise TypeError("size must be an integer")
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         """size must be an integer"""
         elif size < 0:
-        raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         """size must be above 0"""
         else:
-        self.__size = size
+            self.__size = size
 
     def area(self):
         return self.__size ** 2
