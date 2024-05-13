103df382ca79761f50ee262039d16a4d9f1ecb52
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 13:32:21 -0500

    chaged != to is not and added line with now whitespace

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize the Square with size"""
 
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         # size must be an integer
         elif size < 0:
@@ -19,4 +19,3 @@ class Square:
 
     def area(self):
         return self.__size ** 2
-    
