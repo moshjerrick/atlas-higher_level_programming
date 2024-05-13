1c400a65014973a427372906bacebb315c074cdd
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-13 16:54:10 -0500

    added value error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,6 +16,8 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
@@ -27,6 +29,8 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
