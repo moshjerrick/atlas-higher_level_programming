941d5b769614a1bc058927788a4756d5bd088df2
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-14 15:44:16 -0500

    documentation help

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-# Adding ints and floats after casing them to ints"""
-def add_integer(a, b=98):
-    #Add two numbers after converting them to integers.
+"""Module to perform addition of integers and floats."""
 
-    #Args:
-        #a (int, float): The first number to be added.
-        #b (int, float): The second number to be added. Default is 98.
-    if type(a is not type(int, float)):
-        raise TypeError("a must be an integer")
-    if type(b is not type(int, float)):
-        raise TypeError("b must be an integer")
+def add_integer(a, b=98):
+    """Add two numbers after converting them to integers.
+
+    Args:
+        a (int, float): The first number to be added.
+        b (int, float): The second number to be added. Default is 98.
+
+    Raises:
+        TypeError: If either `a` or `b` is not an integer or float.
+
+    Returns:
+        int: The sum of the two numbers as an integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
     return int(a) + int(b)
