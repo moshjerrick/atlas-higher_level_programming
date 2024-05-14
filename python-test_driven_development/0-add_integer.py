53c8e2c587a89b31f7c23d0141123042c03af9f2
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-14 15:40:42 -0500

    used # instead of """

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""Adding ints and floats after casing them to ints"""
+# Adding ints and floats after casing them to ints"""
 def add_integer(a, b=98):
+    #Add two numbers after converting them to integers.
+
+    #Args:
+        #a (int, float): The first number to be added.
+        #b (int, float): The second number to be added. Default is 98.
     if type(a is not type(int, float)):
         raise TypeError("a must be an integer")
     if type(b is not type(int, float)):
