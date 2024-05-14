59c09b1ea5ce60092daf5408e15bd428f3db772e
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-14 17:56:08 -0500

    added some errors

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,5 +3,18 @@
 
 
 def matrix_divided(matrix, div):
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
     if type(matrix) is not type(int, float):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if
+    result_matrix = []
+
+    for row in matrix:
+        divided_row = []
+        for element in row:
+            divided_row.append(element / divisor)
+        result_matrix.append(divided_row)
+    return result_matrix
