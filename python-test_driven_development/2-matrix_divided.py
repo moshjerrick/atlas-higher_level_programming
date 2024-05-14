9e6bf95115ac38e4d2b0fb6208db18349c48f997
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-14 17:47:00 -0500

    adding 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+
+def matrix_divided(matrix, div):
+    if type(matrix) is not type(int, float):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
