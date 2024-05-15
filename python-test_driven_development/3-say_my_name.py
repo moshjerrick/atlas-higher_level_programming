bd4333a4e9c6d5c26522462c19b761d0e71af894
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-14 19:44:44 -0500

    documented func

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """
-Module to Print My name is first name last name
+Module to define first and last name
 """
 
 def say_my_name(first_name, last_name=""):
+    """
+    Function that prints firt name then last name
+
+    Args:
+        first_name = first name
+        last_name = last name
+        """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
