e40dd90d393d0b42162069807e453a504d2bb221
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 14:07:49 -0500

    documentation fix?

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,9 +3,8 @@
 class mylist inherits from list
 """
 class MyList(list):
-    """
-    Class that extends `list`
-    """
+
+    """Class that extends `list`"""
     def print_sorted(self):
         """
         prints in ascending order
