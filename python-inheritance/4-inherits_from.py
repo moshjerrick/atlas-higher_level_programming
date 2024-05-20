4caa55532dac59ba008d40f610522ec125522af9
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 15:07:26 -0500

    returning isinstance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,8 +9,7 @@ def inherits_from(obj, a_class):
     """
     checks to see if object is an instance
     """
-    if isinstance(obj, a_class):
-        return True
-    else:
+    if type(obj) is a_class:
         return False
+    return isinstance(obj, a_class)
     
