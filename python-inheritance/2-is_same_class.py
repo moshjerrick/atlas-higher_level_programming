faf61b6aa30b468b0eabecbd7a760a7c8d0f9e16
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 14:32:08 -0500

    reverting, but its still not using isintance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,4 +7,6 @@ Defining a class-checking function
 def is_same_class(obj, a_class):
     """
      checking if obj is exact instance of a class"""
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    return False
