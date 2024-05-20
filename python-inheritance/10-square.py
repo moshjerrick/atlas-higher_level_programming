ba796539f640b0b38ef8717869369e5953befd28
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 17:38:48 -0500

    added 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a class square
+    that inherits from (9-base_geometry.py).
+"""
+BaseGeometry = __import__('9-base_geometry').BaseGeometry
+
+class Square(BaseGeometry):
+    """ Square Class """
+def __init__(self, size):
+        """ Init a Square """
+        self.interger_validator("size". size)
+        self.__size = size
+        super().__init__(size, size)
