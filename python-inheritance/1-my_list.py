40eb0a9b368e8198dfb0beef8c92d96a3a980948
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 13:39:35 -0500

    adding 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class mylist inherits from list
+"""
+class MyList(list):
+    """
+    type class MyList
+    """
+    def print_sorted(self):
+        """
+        prints in ascending order
+        """
+        print(sorted(list))
