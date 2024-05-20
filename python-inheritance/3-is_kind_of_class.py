b31f72e07e1c78b75d8c948c6f8eaf9fafebbea3
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 14:57:28 -0500

    function doc

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,10 @@ Check for same class or if inherited from
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Checks to see if its a object or instance of another class
+    """
+
     if isinstance(obj, a_class):
         return True
     else:
