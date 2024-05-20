3b17db8364cdd67865ee53915499b896afea5e1f
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 14:09:28 -0500

    indent doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 class mylist inherits from list
 """
 class MyList(list):
-""" Class that extends `list` """
+    """ Class that extends `list` """
     def print_sorted(self):
         """
         prints in ascending order
