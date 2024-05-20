f930610b66a689ef88f754e591a4d872271c3ec3
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 14:30:03 -0500

    dont think this will work 100%

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,6 +7,4 @@ Defining a class-checking function
 def is_same_class(obj, a_class):
     """
      checking if obj is exact instance of a class"""
-    if type(obj) == a_class:
-        return True
-    return False
+    return isinstance(obj, a_class)
