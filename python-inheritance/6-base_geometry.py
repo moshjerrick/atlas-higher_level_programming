bb6b3e8398a741cf92c07d53c977f144e9e70617
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 15:34:12 -0500

    added lines and removed whitespace

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -2,9 +2,10 @@
 """
 Creates class named BaseGeometry
 """
+
+
 class BaseGeometry:
     """ Empty Class """
     def area(self):
         """ raises exceptions"""
         raise Exception("area() is not implemented")
-    
