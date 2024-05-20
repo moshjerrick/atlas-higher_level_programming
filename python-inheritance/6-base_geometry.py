215d18583bee46d04c7cdc0af34b6484153204d5
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 15:33:31 -0500

    added doc

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-
-
+Creates class named BaseGeometry
 """
 class BaseGeometry:
     """ Empty Class """
     def area(self):
+        """ raises exceptions"""
         raise Exception("area() is not implemented")
     
