65fc4d19262ba8ab8c938de0ca8472bfb4b24853
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-20 15:39:08 -0500

    adding 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Creates class named BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Empty Class """
+    def area(self):
+        """ raises exceptions"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """ Validates intergers"""
+        if not isinstance(value, int):
+        raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
