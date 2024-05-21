d6de6a94dafdde6115fc5b60f3ab35bf31c53bf4
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-21 12:12:56 -0500

    added doc to func

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ Defining the write_file function
 
 
 def write_file(filename="", text=""):
+    """ Writing a string of text to a text file """
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
     
