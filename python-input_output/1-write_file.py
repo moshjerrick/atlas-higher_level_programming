d14b255ebef2e1f78bf323c5562dc24119ef0453
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-21 12:11:29 -0500

    adding 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defining the write_file function
+"""
+
+
+def write_file(filename="", text=""):
+    open with(filename, 'w', encoding="utf-8"):
+        return f.write(text)
+    
