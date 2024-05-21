f3245a553d98a006a4815f190d622265403a0c2d
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-21 12:39:12 -0500

    adding 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defining function to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """ Returning JSON representation of an object """
+    return json.dumps(my_obj)
