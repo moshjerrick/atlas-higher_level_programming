3cb1e8176a52e8395440b65fd4cd3949ec90bcdc
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 14:13:25 -0500

    added doc to init

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,9 @@ class Rectangle(Base):
     Rectangle Class inheriting from Base
     """
     def __init__ (self, width, height, x=0, y=0, id=None):
+        """
+        Init class with seom attr
+        """
         super().__init__(id)
         
         self.width = width
