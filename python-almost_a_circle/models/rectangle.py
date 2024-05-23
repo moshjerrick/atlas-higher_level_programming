b0cfcdc135cc184636e00ee1605b18cd6a2c00d8
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 14:45:07 -0500

    value(type) != type(value)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ Class rectangle that inherits from Base
 """
 
 
-from .base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -29,7 +29,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if value(type) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("Width must be >= 0")
@@ -42,7 +42,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if value(type) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be >= 0")
@@ -55,7 +55,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if value(type) is not int:
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value <= 0:
             raise ValueError("x must be >= 0")
@@ -68,8 +68,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if value(type) is not int:
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value <= 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
