2e0da815fd473c28e4bd71effafac8f7cc623ce4
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 14:35:32 -0500

    more empty line management for new betty

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,17 +7,17 @@ Class rectangle that inherits from Base
 from .base import Base
 
 class Rectangle(Base):
+
+
     """
     Rectangle Class inheriting from Base
     """
-
-
     def __init__ (self, width, height, x=0, y=0, id=None):
         """
         Init class with seom attr
         """
         super().__init__(id)
-        
+
         self.width = width
         self.height = height
         self.x = x
@@ -41,7 +41,6 @@ class Rectangle(Base):
     def height(self, value):
         self.__height = value
 
-
     @property
     def x(self):
         """ x getter """
@@ -50,7 +49,6 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         self.__x = value
-
 
     @property
     def y(self):
