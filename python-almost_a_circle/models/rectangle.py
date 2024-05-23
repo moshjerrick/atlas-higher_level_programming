f249a57e514f33f890c02eb714ef7f4e4f11e6db
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 14:51:57 -0500

    capital letter in exception message fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("Width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
