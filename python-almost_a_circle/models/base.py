23b222b5775ae2185ebc3ecd1c80fdb1b61e5509
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 12:55:10 -0500

    added doc to func

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,6 +9,9 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Init id attributes
+        """
         if id is not None:
             self.id = id
         else:
