a67ae7eb0e0c243d8f847b4860d4820e0b93e10f
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 14:34:36 -0500

    added some blank lines, removed tests stuff from bottom

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,8 @@ class Rectangle(Base):
     """
     Rectangle Class inheriting from Base
     """
+
+
     def __init__ (self, width, height, x=0, y=0, id=None):
         """
         Init class with seom attr
@@ -58,16 +60,3 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
-
-
-
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 2)
-    print(r1.id)
-
-    r2 = Rectangle(2, 10)
-    print(r2.id)
-
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
