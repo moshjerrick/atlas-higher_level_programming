b0a1e917893a3584151d9684b82d963c1ed8ca2f
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-23 14:12:28 -0500

    pushing rectangle because i havnt yet

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Class rectangle that inherits from Base
+"""
+
+from base import Base
+
+class Rectangle(Base):
+    """
+    Rectangle Class inheriting from Base
+    """
+    def __init__ (self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
