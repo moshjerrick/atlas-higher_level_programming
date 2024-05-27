15e66e827a28c9162b5431a0060e87735f6f415b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-27 16:47:06 -0500

    removed tests

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,22 +100,3 @@ class Rectangle(Base):
             if i < len(args):
                 setattr(self, attr, args[i])
 
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 10, 10, 10)
-    print(r1)
-
-    r1.update(89)
-    print(r1)
-
-    r1.update(89, 2)
-    print(r1)
-
-    r1.update(89, 2, 3)
-    print(r1)
-
-    r1.update(89, 2, 3, 4)
-    print(r1)
-
-    r1.update(89, 2, 3, 4, 5)
-    print(r1)
