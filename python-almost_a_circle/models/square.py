6d8ee2388a6847607b05168c4c9610b55af62ed6
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-28 12:12:41 -0500

    adding square file

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class Square that inherits from Rectangle
+"""
+
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None)
