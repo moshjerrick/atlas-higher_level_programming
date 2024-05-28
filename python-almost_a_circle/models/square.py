b48cef507a4f84f6398e40ca37fdeee93d581e96
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-28 12:24:16 -0500

    call super class, width and height are now size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,4 +8,8 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
 
-    def __init__(self, size, x=0, y=0, id=None)
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Square class constructor
+        """
+       super().__init__(size, size, x, y, id)
