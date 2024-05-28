cbae2a0b42b55be5b5c8de06f40d9bdddf91bf02
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-28 17:10:25 -0500

    ok, now dict the square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -77,3 +77,9 @@ class Square(Rectangle):
                     self.y = value
                 else:
                     break
+    def to_dictionary(self):
+        """
+        Returns the Squares's dict
+        """
+        s_dict = {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
+        return s_dict
