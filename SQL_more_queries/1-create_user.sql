a57eaa9669ef834a1f702ecd9dce8290be58dcb2
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-17 14:01:47 -0500

    actually actually fixed ;

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Creating user for server
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'password_0d_1_pwd;
+    IDENTIFIED BY 'password_0d_1_pwd';
 GRANT ALL PRIVILEGES
     ON *.*
     TO 'user_0d_1'@'localhost'
