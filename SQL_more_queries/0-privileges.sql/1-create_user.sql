c0b1a63e56c622116434a795449225a9b84eed8f
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-17 13:54:48 -0500

    ADDED 1

---
+++ SQL_more_queries/0-privileges.sql/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Creating user for server
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'password_0d_1_pwd;
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'password_0d_1_pwd;
+    
