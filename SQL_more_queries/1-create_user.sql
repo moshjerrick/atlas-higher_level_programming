1703373be45eaf88ea3504665e7bcde7b29042c4
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-17 14:11:15 -0500

    what am i do

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,11 @@
 -- Creating user for server
 CREATE USER
+CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
--- GRANT ALL PRIVILEGES
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-    FLUSH PRIVILEGES;
+-- Grant all privileges to user
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
