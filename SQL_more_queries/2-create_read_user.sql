9bf30fc6f40c31cfe20ed13d05cb83e2e1f2793a
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-17 15:06:16 -0500

    removed lots of '

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create the MySQL server user user_0d_2
 CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 --Granting priviledges to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
