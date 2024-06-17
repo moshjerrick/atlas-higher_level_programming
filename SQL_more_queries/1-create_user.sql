c0f3a337d4ff434fc5cd73558f1285cb5aa1fbd2
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-17 14:06:19 -0500

    user not password in password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Creating user for server
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'password_0d_1_pwd';
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
     ON *.*
     TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'password_0d_1_pwd';
+    IDENTIFIED BY 'user_0d_1_pwd';
     FLUSH PRIVILEGES;
