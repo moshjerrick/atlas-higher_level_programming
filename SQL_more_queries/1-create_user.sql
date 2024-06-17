8b920c2ca1a32bc2862b1a0c631a0c8462a962b2
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-17 13:59:54 -0500

    added ;

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,6 +4,6 @@ CREATE USER
     IDENTIFIED BY 'password_0d_1_pwd;
 GRANT ALL PRIVILEGES
     ON *.*
-    TO 'user_0d_1'@'localhost'
+    TO 'user_0d_1'@'localhost';
     IDENTIFIED BY 'password_0d_1_pwd;
     
