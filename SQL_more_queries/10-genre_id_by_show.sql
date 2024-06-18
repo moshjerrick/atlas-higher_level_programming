829233d7d9c6e52626fa4246601d82a1185b5dfa
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-18 12:37:57 -0500

    added comma in select  clause

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- import database dump from hbtn_0d_tvshows
-SELECT tv_shows.title tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_shows_genres.genre_id
     FROM tv_shows
     LEFT JOIN tv_shows_genres
     ON tv_shows.id = tv_shows_genres.show_id
