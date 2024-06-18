d3fcd228c125174054cae689e80f430d3f1cc27c
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-18 11:41:24 -0500

    is this even different?

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,9 @@
 -- Lists all cities in california found in hbtn_0d_usa
-SELECT id, city FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+SELECT id, city
+FROM cities
+WHERE state_id IN (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id;
