7d06feadc176601e37c7e4a1727aad64ac79de04
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-18 16:45:07 -0500

    was working on wrong file....

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,4 +2,6 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ORDER BY tb_shows.title, tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
