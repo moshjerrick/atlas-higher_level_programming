cc095238adbc41054c1877678608bb42f0dfdf94
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-18 12:18:13 -0500

    removed comma

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- List all cities in data base
-SELECT `cities`.`id`, `cities`.`name`, `states`.`name`,
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
   FROM `cities`
        JOIN `states`
        ON `cities`.`state_id` = `states`.`id`
