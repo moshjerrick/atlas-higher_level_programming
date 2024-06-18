fb1d69a236983ee7a51e5a8a74fc5648f6bf3f55
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-18 12:47:27 -0500

    REMOVED JOIN

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- import database dump from hbtn_0d_tvshows
 SELECT tv_shows.title, tv_shows_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
+FROM tv_shows, tv_shows_genres
+WHERE tv_shows.id = tv_shows_genres.show_id
 ORDER BY tv_shows.title, tv_shows_genres.genre_id;
