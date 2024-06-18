e72180f154f7b82a6d790ceeea2af92a054a4470
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-06-18 11:12:04 -0500

    added 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name CHAR(50) NOT NULL
+);
