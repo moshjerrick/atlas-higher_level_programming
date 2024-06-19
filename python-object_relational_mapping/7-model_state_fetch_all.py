0fe01c44550e1e9334201b9a799adb2d430fe360
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 18:16:07 -0500

    add 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+List all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Acces to the database
+    """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"  
+                #Creates engine instance to manage
+                #connections to the database
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                #Use command line args to form database connections string
+                        pool_pre_ping=True)  #tests connection for liveness
+    Base.metadata.create_all(engine)  
+    #Creates all tables defined in the 'Base' metadata, like 'State'
+    Session = sessionmaker(bind=engine)  #Creates a 'Session' class
+    session = Session()  #Creates a 'Session' instance
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
