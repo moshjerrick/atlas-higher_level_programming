7fc9db93eb27fae128adba04943f22cc2c52b89c
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 18:30:49 -0500

    # space?

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Acces to the database
     """
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"  
-                #Creates engine instance to manage
+                # Creates engine instance to manage
                 #connections to the database
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                 #Use command line args to form database connections string
