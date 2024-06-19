034671ce358aa110a25cb2f3f5709e4dc89ffe6a
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 13:25:00 -0500

    added 13 and readmen

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+# script that lists all states from the database hbtn_0e_0_usa
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor() # create a cursor
+    cursor.execute("SELECT * FROM states ORDER BY id") # execute the query to select all records from the states table
+    states = cursor.fetchall() # fetch all the records and store in states variable
+    for state in states: # iterates (state) in the states list
+        print(state) # prints each state which represents a row from the states table
+    cursor.close()
+    db.close()
+    
