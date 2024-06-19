3f46c24eb4e27db5b59294183a55ad845cbaa426
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 16:28:31 -0500

    added 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Taking in arguements and displaying all values in 
+states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    
+    cursor=db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4]))
+    state = cursor.fetchall()
+    for states in state:
+        print(states)
+        
