9ef12b31b4450f991b2bc2cf2a5dd00ddeaecd2c
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 16:31:43 -0500

    added whitespace

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cursor=db.cursor()
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4]))
     state = cursor.fetchall()
     for states in state:
