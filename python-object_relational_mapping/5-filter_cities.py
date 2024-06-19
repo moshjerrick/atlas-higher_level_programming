f5f319e250429f27a815f6aeab33943bca1a7cf3
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 17:33:25 -0500

    added back the where %s

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name \
-        FROM cities JOIN states ON cities.state_id = states.id ORDER\
-            BY cities.id")
+        FROM cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
     states = cursor.fetchall()
     if states is not None:
         print(", ".join([state[1] for state in states]))
