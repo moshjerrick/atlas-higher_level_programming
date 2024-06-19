c3493b762bdac108a6201c6c7d86cf3e24994223
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 18:03:46 -0500

    mo betty

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -18,7 +18,7 @@ class State(Base):
         id (int): The State id of the class
         name (str): The State name of the class
     """
-    __tablename__ = 'states' 
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
