8e996463f51c501e1ed2164cfd3eb1f7c9bcadda
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 17:01:07 -0500

    betty

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,10 +8,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor=db.cursor()
+    cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name \
-        FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+        FROM cities JOIN states ON cities.state_id = states.id ORDER\
+            BY cities.id")
     states = cursor.fetchall()
     for state in states:
         print(state)
-    
