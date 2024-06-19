02f68ded686eec8a6ee57091821ad4159576ff2e
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 15:47:18 -0500

    i hate betty

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
- lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+ lists all states with a name starting with N 
+ (upper N) from the database hbtn_0e_0_usa
 """
 
 import sys
@@ -11,7 +12,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()  # create a cursor
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-    # execute the query to select all records from the states table where the name starts with N
+    # execute the query to select all records from
+    # the states table where the name starts with N
     states = cursor.fetchall()
     for state in states:  # iterates (state) in the states list
         print(state)
