31c6c7b2747927799e36d96bbbe050c965b81d25
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 16:30:50 -0500

    chore: Fix typo in code comments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Taking in arguements and displaying all values in 
+Taking in arguements and displaying all values in
 states table of hbtn_0e_0_usa where name matches the argument
 """
 
@@ -9,10 +9,9 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    
+
     cursor=db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4]))
     state = cursor.fetchall()
     for states in state:
         print(states)
-        
