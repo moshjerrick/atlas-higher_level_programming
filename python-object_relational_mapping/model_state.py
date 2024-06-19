ac2d8eb43baf9a947ae65f8b1a3280361038c247
Josh Merrick <moshjerrick@gmail.com>
2024-06-19 17:58:34 -0500

    spelling column right

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -19,5 +19,5 @@ class state(base):
     """
     __tablename__ = 'states' 
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Columb(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
     
