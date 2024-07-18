5fe6c5c0da0e3febc883ddd18a0c57217ae44223
Josh Merrick <moshjerrick@gmail.com>
2024-07-18 12:46:43 -0500

    chore: Update JavaScript warm-up files with shebang and newline at end of file

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-function printBasedOnArguments(...args) {
-    switch (args.length) {
-        case 0:
-            console.log('No argument');
-            break;
-        case 1:
-            console.log('Argument found');
-            break;
-        case 2:
-            console.log('Arguments found');
-            break;
-    }
-}
+// Prints message based on number of arguments
