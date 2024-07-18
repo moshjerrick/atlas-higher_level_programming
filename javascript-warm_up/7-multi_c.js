03e2a317804503e399657a80ab5f5125f9074a65
Josh Merrick <moshjerrick@gmail.com>
2024-07-18 14:45:10 -0500

    started 7, not finished/working

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints x times "C is fun"
+const [,, x] = process.argv;
+const num = parseInt(x);
+const langs = ['C is fun'];
+if (isNaN(x) {
+  console.log('Missing number of occurrences');
+}
