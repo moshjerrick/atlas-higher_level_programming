0a7c6f902e099ce9ac73d4c0b66c21009adc3619
Josh Merrick <moshjerrick@gmail.com>
2024-07-18 14:04:01 -0500

    added a space after for

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 // prints 3 lines by using an array of string and a loop
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(const lang of langs) {
+for (const lang of langs) {
   console.log(lang);
 }
