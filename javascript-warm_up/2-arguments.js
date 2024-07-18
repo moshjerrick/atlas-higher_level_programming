2fb19c9a621dc7faa1d58543d85d52398e971c3b
Josh Merrick <moshjerrick@gmail.com>
2024-07-18 12:53:37 -0500

    helps if youre wrking in the right file

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 // Prints message based on number of arguments
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
