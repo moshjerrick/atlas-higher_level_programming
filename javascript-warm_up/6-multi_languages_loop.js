b284347ab918f2ebb21850726c641143aae21b0b
Josh Merrick <moshjerrick@gmail.com>
2024-07-18 14:00:57 -0500

    added working 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints 3 lines by using an array of string and a loop
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for(const lang of langs) {
+  console.log(lang);
+}
