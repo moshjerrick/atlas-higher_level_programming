e7b84ab3c5034605d99370a6ba41ddfd4e6a1fd3
Josh Merrick <moshjerrick@gmail.com>
2024-07-18 23:28:58 -0500

    Refactor JavaScript warm-up file to use destructuring assignment for first argument

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const [,, x] = process.argv;
 const num = parseInt(x);
 if (isNaN(x)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < num; i++) {
-        console.log('X'.repeat(num));
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 }
