c20532c0246299473527732d967b5a7c6840d206
Josh Merrick <moshjerrick@gmail.com>
2024-07-22 11:08:04 -0500

    Refactor JavaScript warm-up file to use destructuring assignment for first argument

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that computes and prints a factorial
+function factorial (n) {
+    if (n > 0) {
+      return (n * factorial(n - 1));
+    } else if (n === 0) {
+      return (1);
+    } else {
+      return (1);
+    }
+  }
+  const number = Number(process.argv[2]);
+  console.log(factorial(number));
