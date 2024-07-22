ef69575b61889cd5cf4d13b8698727253901af70
Josh Merrick <moshjerrick@gmail.com>
2024-07-22 13:06:21 -0500

    changed sort to descending order

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const args = process.argv.slice(2).map(Number);
 if (args.length < 2) {
   console.log(0);
 } else {
-    args.sort((a, b) => a - b);
+    args.sort((a, b) => b - a);
     console.log(args[1]);
 }
