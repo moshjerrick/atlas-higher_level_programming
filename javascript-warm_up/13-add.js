7bd1d7e73a336a05abee9993f192d1f965da4906
Josh Merrick <moshjerrick@gmail.com>
2024-07-22 13:14:48 -0500

    Refactor JavaScript warm-up file to use destructuring assignment for first argument

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints the addition of two ints
+const [,, a, b] = process.argv;
+const num1 = parseInt(a);
+const num2 = parseInt(b);
+function add (a, b) {
+  return a + b;
+}
+console.log(add(num1, num2));
