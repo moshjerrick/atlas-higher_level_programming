ef2d5edbc37a6edeeabea8ab3c4a089f141e653e
Josh Merrick <moshjerrick@gmail.com>
2024-07-22 11:09:49 -0500

    indent

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,13 +2,12 @@
 // Script that computes and prints a factorial
 function factorial (n) {
     if (n > 0) {
-      return (n * factorial(n - 1));
+        return (n * factorial(n - 1));
     } else if (n === 0) {
-      return (1);
+        return (1);
     } else {
-      return (1);
+        return (1);
     }
   }
   const number = Number(process.argv[2]);
   console.log(factorial(number));
-  
