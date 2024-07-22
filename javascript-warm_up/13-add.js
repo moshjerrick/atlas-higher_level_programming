eb4ac7c12f914dabe347fcaa277c2ca9de96a2ac
Josh Merrick <moshjerrick@gmail.com>
2024-07-22 13:16:33 -0500

    ok, copying and pasting my old answer didnt work

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 // prints the addition of two ints
-const [,, a, b] = process.argv;
-const num1 = parseInt(a);
-const num2 = parseInt(b);
-function add (a, b) {
-  return a + b;
-}
-console.log(add(num1, num2));
+module.exports = {
+    add: (a, b) => a + b
+  };
