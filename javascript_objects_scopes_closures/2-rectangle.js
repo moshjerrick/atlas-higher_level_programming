31c36d2781e40f0ddf4172444b36d8dd97b7882a
Josh Merrick <moshjerrick@gmail.com>
2024-07-22 14:37:53 -0500

    Refactor Rectangle class constructor to handle non-zero and positive width and height values

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 // class Rectangle that defines a rectangle
-class Rectangle {
-    constructor (w, h) {
-      if (w !== 0 && h !== 0) && (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0)
+    this.width = w;
+    this.height = h;
   }
-  
-module.exports = Rectangle;
-  
+};
