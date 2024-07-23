2917003fa4d554c0185a94c2daa82247a3b8be31
Josh Merrick <moshjerrick@gmail.com>
2024-07-23 14:16:01 -0500

    square inheriting from square for 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// class Square that defines a square and inherits from Square
+const Square = require('./5-square');
+
+module.exprot = class Square extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
