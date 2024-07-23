137efe1c30f8c2239f1acbafeb6e8c182ab9ba5c
Josh Merrick <moshjerrick@gmail.com>
2024-07-23 14:22:16 -0500

    Refactor Square class to inherit from parentSquare

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // class Square that defines a square and inherits from Square
-const Square = require('./5-square');
+const parentSquare = require('./5-square');
 
 module.export = class Square extends parentSquare {
   charPrint (c) {
