ce3e57dc8c5932c29231af4e45dcce77e5640798
Josh Merrick <moshjerrick@gmail.com>
2024-07-23 14:36:51 -0500

    feat: Add function to count occurrences in a list

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let count = 0;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+    return count;
+};
