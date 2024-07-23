0b7ead74b630886280ce8c89d0e5eb052399af4e
Josh Merrick <moshjerrick@gmail.com>
2024-07-23 14:47:35 -0500

    if undefined, make it 0

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 // funtion that prints the number of args alrady printed and the new arg value
 exports.logMe = function (item) {
+    if (this.count === undefined) {
+        this.count = 0;
+    }
   console.log(this.count + ': ' + item);
   this.count++;
 };
