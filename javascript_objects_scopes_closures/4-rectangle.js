1767f63f67870d8c59372fda8cae78868e1c528c
Josh Merrick <moshjerrick@gmail.com>
2024-07-23 12:05:02 -0500

    semistandard --fix

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
 // class Rectangle that defines a rectangle
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print () {
-        let i = 0;
-        for (i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-            }
-        }
-    rotate () {
-        let temp = this.width;
-        this.width = this.height;
-        this.height = temp;
+  print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
-  };
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
