2dcdcce98cc09786b53e59910a61bb72cdb5696e
Josh Merrick <moshjerrick@gmail.com>
2024-07-25 14:32:28 -0500

    created 1, not working

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//  a script that writes a string to a file.
+const fs = require('fs');
+const filePath = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(filePath, 'utf-8', (err, string) => {
+    if (err) {
+        console.log(err);
+      } else {
+        console.log(string);
+      }
+    });
