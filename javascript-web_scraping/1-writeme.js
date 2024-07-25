ef225d85126d9ae61d184e7d78e0f3f7de8b64a0
Josh Merrick <moshjerrick@gmail.com>
2024-07-25 14:42:02 -0500

    semistandard --fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 fs.writeFile(filePath, string, 'utf8', (err) => {
-    if (err) {
-        console.log(err);
-      } else {
-        //console.log(string);
-      }
-    });
+  if (err) {
+    console.log(err);
+  } else {
+    // console.log(string);
+  }
+});
