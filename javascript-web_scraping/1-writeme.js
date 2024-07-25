be8a3b20b9ada6d0a0aac7701b2abfc00792f725
Josh Merrick <moshjerrick@gmail.com>
2024-07-25 14:35:35 -0500

    removed - from utf-8 because ai told me to

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
-fs.writeFile(filePath, string, 'utf-8', (err) => {
+fs.writeFile(filePath, string, 'utf8', (err) => {
     if (err) {
         console.log(err);
       } else {
