7c206901085a72265067742c17067995fdc3ecdd
Josh Merrick <moshjerrick@gmail.com>
2024-07-26 12:33:16 -0500

    cant remember if i pushed this yesterday before i left....

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//  script that prints the title of a Star Wars movie where the episode number matches a given integer.
+const request = require('request');
+const movieID = process.argv[2];
+const URL = 'https://swapi-api.hbtn.io/api/films/:id' + movieID;
+
+request.get(URL, (err, response, body) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.parse(body).title);
+    }
+    }
+);
