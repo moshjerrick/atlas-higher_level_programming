1356f08b5a85dd9a2ba0b1c6117017cbec85da95
Josh Merrick <moshjerrick@gmail.com>
2024-07-26 12:38:53 -0500

    semistandard fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,9 @@ const movieID = process.argv[2];
 const URL = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
 request.get(URL, (err, response, body) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(JSON.parse(body).title);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
