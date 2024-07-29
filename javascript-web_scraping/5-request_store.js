06e63bc33b6c17d109bb3cbcdef9c5f1ed8dbfd8
Josh Merrick <moshjerrick@gmail.com>
2024-07-29 11:03:18 -0500

    requested, then wrote it, didnt just attempt to write

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // script that writes the contents of a webpage to a file.
-const require = require('request');
+const request = require('request');
 const fs = require('fs');
-const { request } = require('http');
 
 const URL = process.argv[2];
 const filePath = process.argv[3];
@@ -10,5 +9,12 @@ const filePath = process.argv[3];
 request.get(URL, (err, response, body) => {
   if (err) {
     console.log(err);
-  } else {
-    fs.writeFile
+    return;
+  }
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  }
+  );
+});
